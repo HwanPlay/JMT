ef02e6d3835f711fb4b8cc34f35d2da787002763
이태환 <lwh1992@naver.com>
2020-07-22 12:06:48 +0900

    electron|insert editor

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,6 +4,19 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 
+
+// 태환 bootstrap-vue
+import '../src/assets/sass/main.scss'
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
 Vue.config.productionTip = false
 
 new Vue({
