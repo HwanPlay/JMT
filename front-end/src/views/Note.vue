8995090973de5e8c89e83d19222b4ee75c073cb3
이태환 <lwh1992@naver.com>
2020-08-05 09:26:57 +0900

    editor axios

--- front-end/src/views/Note.vue
+++ front-end/src/views/Note.vue
@@ -1,7 +1,7 @@
 <template>
   <b-container style="margin-top: 4rem" fluid>
     <b-row>
-      <NoteSearch :group_list="group_list" @onGetNote="GetNote" />
+      <NoteSearch :group_list="group_list" @onGetNoteList="getNoteList" :notes_list="notes_list" />
       <NoteEditor :receivedHTML="receivedHTML" @onUploadHTML="UploadHTML" />
     </b-row>
   </b-container>
@@ -12,7 +12,7 @@ import NoteEditor from '../components/Note/NoteEditor.vue';
 import NoteSearch from '../components/Note/NoteSearch.vue';
 
 import axios from 'axios';
-const SERVER_URL = 'https://localhost:8080/';
+const SERVER_URL = 'http://localhost:8080/';
 
 export default {
   name: 'Note',
@@ -21,78 +21,41 @@ export default {
     NoteSearch,
   },
   data() {
-    return {
-      group_list: [
-        {
-          group_id: 1,
-          group_title: '그룹 이름',
-          group_conf_data: [
-            {
-              date: '1월 1일',
-              note_list: [
-                {
-                  note_id: 3,
-                  note_title: '주제A',
-                },
-                {
-                  note_id: 5,
-                  note_title: '주제B',
-                },
-              ],
-            },
-            {
-              date: '1월5일',
-              note_list: [
-                {
-                  note_id: 4,
-                  note_title: '주제c',
-                },
-                {
-                  note_id: 7,
-                  note_title: '주제d',
-                },
-              ],
-            },
-          ],
-        },
-      ],
-      receivedHTML: '',
-      dataHTML: '',
-      group_list: Object,
+    return {      
+      group_list: Array,
+      notes_list: Array,
     };
   },
   methods: {
-    getNoteList() {
-      axios
-        .post(SERVER_URL, 'DATA', 'CONFIG')
-        .then((res) => {
-          console.log(res);
-          this.NoteList = res;
-        })
-        .catch((err) => {
-          console.error(err);
-        });
-    },
-    UploadHTML(res) {
-      console.log(res);
-      // axios로 요청 보낼것
-    },
-
     // api 추가
     get_group_list() {
       const URL = 'videoconference/api/group/gethost/';
-      const ID = 'lwh1992@naver.com';
+      const ID = 'lwh1992@naver.com/';
 
       axios
         .get(SERVER_URL + URL + ID)
-        .then((res)=>{
-          this.group_list = res;
+        .then((res) => {
+          console.log(res.data.groups);
+          this.group_list = res.data.groups;
         })
-        .catch((err)=>console.error(err));
+        .catch((err) => console.error(err));
+    },
+    getNoteList(groupId) {
+      console.log(groupId);
+      
+      const URL = 'videoconference/api/note/get/group/';
+      const groupNoAndId = '1/lwh1992@naver.com';
+      axios
+        .get(SERVER_URL + URL + groupNoAndId)
+        .then((res)=>{
+          this.notes_list = res.data.notes;
+          console.log(res);
+        })
+        .catch((err) => console.error(err));
     },
   },
   mounted() {
-    // this.get_group_list();
-  }
+    this.get_group_list();
+  },
 };
 </script>
