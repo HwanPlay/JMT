bce6e49cae9cbd1463ff7e26f2aafbde9ee06555
JinA <JinA@DESKTOP-CN5Q1SF>
2020-08-05 15:05:46 +0900

    [Back] logout 수정

--- backend/src/main/java/com/ssafy/videoconference/config/security/handler/CustomLogoutHandler.java
+++ backend/src/main/java/com/ssafy/videoconference/config/security/handler/CustomLogoutHandler.java
@@ -11,27 +11,23 @@ import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 import com.ssafy.videoconference.model.user.bean.UserDetail;
 
-public class CustomLogoutHandler implements LogoutHandler{
+public class CustomLogoutHandler implements LogoutHandler {
 
 	@Autowired
-    RedisTemplate<String,Object> redisTemplate;
+	RedisTemplate<String, Object> redisTemplate;
 
-	
 	@Override
 	public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-		 
+
 		UserDetail userDetail = (UserDetail) authentication.getPrincipal();
-		String accessToken = userDetail.getId()+"access_Token";
-		String refreshToken = userDetail.getId()+"refresh_Token";
+		String accessToken = userDetail.getId() + "_accessToken";
+		String refreshToken = userDetail.getId() + "_refreshToken";
 		
-		if (redisTemplate.opsForValue().get(accessToken) != null) {
-             redisTemplate.delete(accessToken);
-        }
+		System.out.println("logout - redis delete");
 		
-		if (redisTemplate.opsForValue().get(refreshToken) != null) {
-            redisTemplate.delete(refreshToken);
-       }
-	
+		redisTemplate.delete(accessToken);
+		redisTemplate.delete(refreshToken);
+
 		response.setStatus(HttpStatus.OK.value());
 	}
 
