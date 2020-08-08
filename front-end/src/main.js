8a04c522fd9e95644add94ad9d93f95cb4712296
yeongjin <wjddudwls13@naver.com>
2020-08-08 13:06:54 +0900

    conference css

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -8,7 +8,7 @@ import '@mdi/font/css/materialdesignicons.css';
 
 import '../src/assets/sass/main.scss';
 
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import { BootstrapVue, IconsPlugin ,BootstrapVueIcons } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
@@ -16,6 +16,8 @@ import 'bootstrap-vue/dist/bootstrap-vue.css';
 import vueMoment from 'vue-moment';
 // Install BootstrapVue
 Vue.use(BootstrapVue);
+
+Vue.use(BootstrapVueIcons);
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin);
 Vue.use(vueMoment);
--- front-end/src/views/Conference.vue
+++ front-end/src/views/Conference.vue
@@ -5,9 +5,9 @@
         <videochat />
       </div>
     </div>
-    <div class="main_2 main_common">
+    <!-- <div class="main_2 main_common">
       <Sharescreen />
-    </div>
+    </div> -->
     <div class="main_3 main_common">
       <Webrtc />
     </div>
@@ -16,14 +16,14 @@
 
 <script>
 import Videochat from '../components/Conference/Videochat.vue';
-import Sharescreen from '../components/Conference/Sharescreen.vue';
+// import Sharescreen from '../components/Conference/Sharescreen.vue';
 import Webrtc from '../components/Conference/webrtc.vue';
 
 export default {
   name: 'Conference',
   components: {
     Videochat,
-    Sharescreen,
+    // Sharescreen,
     Webrtc,
   },
 };
