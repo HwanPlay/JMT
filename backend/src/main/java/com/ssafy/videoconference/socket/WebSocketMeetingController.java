720e2f1656214e90957d9172d57769946f740321
ltk3934 <ltk3934@daum.net>
2020-08-11 02:38:02 +0900

    Add ServerSocket

--- backend/src/main/java/com/ssafy/videoconference/socket/WebSocketMeetingController.java
+++ backend/src/main/java/com/ssafy/videoconference/socket/WebSocketMeetingController.java
@@ -1,5 +1,19 @@
 package com.ssafy.videoconference.socket;
 
-public class WebSocketMeetingController {
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+public class WebSocketMeetingController {
+	
+	private final SimpMessageSendingOperations messagingTemplate;
+	
+	@MessageMapping("/meeting")
+	public void message(MeetingMessage message) {
+		messagingTemplate.convertAndSend("/sub/meeting" + message.getGroupNo(), message);
+	}
 }
