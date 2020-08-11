8e43b108e62648ef790ec141e511e39ce63f49be
taekyung2 <ltk3934@daum.net>
2020-08-11 10:48:24 +0900

    Edit redisconfig

--- backend/src/main/java/com/ssafy/videoconference/config/RedisConfig.java
+++ backend/src/main/java/com/ssafy/videoconference/config/RedisConfig.java
@@ -1,21 +1,39 @@
 package com.ssafy.videoconference.config;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.convert.RedisData;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-@Configuration
-public class RedisConfig {
 
+
+@Configuration
+@PropertySource("application.properties")
+public class RedisConfig {
+	@Value("${spring.redis.host}")
+	private String redisHost;
+	
+	@Value("${spring.redis.port}")
+	private int redisPort;
+	
+	
+	
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory();
+    	
+    	RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+    	redisStandaloneConfiguration.setHostName(redisHost);
+    	redisStandaloneConfiguration.setPort(redisPort);
+    	
+    	
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
         return lettuceConnectionFactory;
     }
     @Bean
