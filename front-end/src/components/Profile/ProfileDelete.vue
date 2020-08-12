e57c8443215b37139a55e383c01bff43c0abf2d3
이태환 <lwh1992@naver.com>
2020-08-12 22:56:29 +0900

    del account 틀

---
+++ front-end/src/components/Profile/ProfileDelete.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="text-center">
+    <v-dialog v-model="dialog" >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn class="text-center" text small color="error" v-bind="attrs" v-on="on">Delete Account</v-btn>
+      </template>
+
+      <v-card style="width: 500px">
+        <v-card-title class="headline grey lighten-2">DELETE ACCOUNT</v-card-title>
+
+        <v-card-text>Input Your Password</v-card-text>
+        <v-text-field
+            v-model="password"
+            label="Password"
+            required
+          ></v-text-field>
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" text @click="SubmitDeleteAccount">Submit</v-btn>
+          <v-btn color="danger" text @click="dialog = false">Cancel</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import SERVER from '../../api/spring.js';
+
+export default {
+  name: 'ProfileDelete',
+  data() {
+    return {
+      dialog: false,
+      password: ''
+    };
+  },
+  methods: {
+    SubmitDeleteAccount() {
+      this.dialog = false;
+      axios.delete(SERVER.URL + '/user/delUser/' + this.$store.state.userId).then().catch();
+    }
+  }
+};
+</script>
+
+<style scoped>
+.btn-box {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
--- front-end/src/views/Profile.vue
+++ front-end/src/views/Profile.vue
@@ -3,18 +3,20 @@
     <ProfileBasicCard />
     <div style="height: 2rem;"></div>
     <ProfilePassword />
+    <ProfileDelete />
   </v-container>
 </template>
 
 <script>
 import ProfileBasicCard from '../components/Profile/ProfileBasicCard.vue';
 import ProfilePassword from '../components/Profile/ProfilePassword.vue';
-
+import ProfileDelete from '../components/Profile/ProfileDelete.vue';
 export default {
   name: 'Profile',
   components: {
     ProfileBasicCard,
-    ProfilePassword
+    ProfilePassword,
+    ProfileDelete,
   }
 };
 </script>
