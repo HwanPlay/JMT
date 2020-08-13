158e829a128da3740838cc6cd4f0fca9892b9286
이태환 <lwh1992@naver.com>
2020-08-13 10:30:01 +0900

    Resolved S03P13B102-107

--- front-end/src/components/Profile/ProfileDelete.vue
+++ front-end/src/components/Profile/ProfileDelete.vue
@@ -1,25 +1,33 @@
 <template>
   <div class="text-center">
-    <v-dialog v-model="dialog" >
+    <v-dialog v-model="dialog" width="500">
       <template v-slot:activator="{ on, attrs }">
         <v-btn class="text-center" text small color="error" v-bind="attrs" v-on="on">Delete Account</v-btn>
       </template>
 
-      <v-card style="width: 500px">
+      <v-card>
         <v-card-title class="headline grey lighten-2">DELETE ACCOUNT</v-card-title>
 
         <v-card-text>Input Your Password</v-card-text>
-        <v-text-field
-            v-model="password"
-            label="Password"
-            required
-          ></v-text-field>
+        <v-card-text>
+          <div class="text--primary">
+          logout 됩니다.
+          {{ alert_text }}
+          </div>
+        </v-card-text>
+
+        
+
+        <v-card-text>
+          <v-text-field type="password" v-model="password" label="Password" required></v-text-field>
+        </v-card-text>
+
         <v-divider></v-divider>
 
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn color="primary" text @click="SubmitDeleteAccount">Submit</v-btn>
-          <v-btn color="danger" text @click="dialog = false">Cancel</v-btn>
+          <v-btn color="red" text @click="dialog = false">Cancel</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -35,15 +43,32 @@ export default {
   data() {
     return {
       dialog: false,
-      password: ''
+      password: '',
+      alert_text: '',
     };
   },
   methods: {
     SubmitDeleteAccount() {
-      this.dialog = false;
-      axios.delete(SERVER.URL + '/user/delUser/' + this.$store.state.userId).then().catch();
+      // this.dialog = false;
+      this.clearPassword();
+      axios
+        .delete(SERVER.URL + '/user/delUser/' + this.$store.state.userId)
+        .then((res) => {
+          if (res.data === 'success') {
+            // 로그아웃 하기.
+            this.dialog = false;
+            this.$store.dispatch('logout');
+          } else {
+            this.alert_text = '비밀번호가 틀렸습니다. 다시 입력하세요.';
+            // 비번 틀렸다고 보여주기.
+          }
+        })
+        .catch();
+    },
+    clearPassword() {
+      this.password = '';
     }
-  }
+  },
 };
 </script>
 
