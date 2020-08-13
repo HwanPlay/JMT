00cfd57475442e071f625f54bc9f91eb2910dd3a
이태환 <lwh1992@naver.com>
2020-08-13 20:48:53 +0900

    search bar color

--- front-end/src/components/Note/NoteSearch.vue
+++ front-end/src/components/Note/NoteSearch.vue
@@ -1,24 +1,68 @@
 <template>
-  <b-col cols="3" class="rounded border border-secondary">
-    <h2 class="text-center">Groups</h2>
-    
-    <NoteSearchSidebar
-      :group_list="group_list"
-      :received_note_list="received_note_list"
-      @onGetNoteList="getNoteList"
-      @onGetNoteHTML="getNoteHTML"
-    />
+  <b-col cols="3" style="padding: 0" class="rounded">
+    <v-navigation-drawer height="100%" permanent>
+      <div id="myProfile">
+        <div>
+          <img id="myImage" src="../../assets/profile/profile1.jpg" />
+        </div>
+        <v-list-item-content class="item-content">
+          <v-list-item-title class="myName">
+            <h2 style="color : white">{{ $store.state.myName }}</h2>
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            <h5 style="color : white">{{ $store.state.userId }}</h5>
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </div>
+      <v-divider></v-divider>
+
+      <div v-for="group in group_list" :key="group.groupNo">
+        <div class="rounded group_btn">
+          <b-button
+            class="text-left groupNameText"
+            block
+            variant="white"
+            @click="getNoteList(group.groupNo)"
+            v-b-toggle.sidebar-backdrop
+          >
+            <v-icon style="margin-top : 5px; margin-left : 10px;  margin-right : 20px;">mdi-account-multiple</v-icon>
+            <h3 style="display:inline">{{group.groupName}}</h3>
+          </b-button>
+        </div>
+      </div>
+    </v-navigation-drawer>
+
+    <b-sidebar
+      id="sidebar-backdrop"
+      backdrop
+      shadow
+      no-header
+      width="25rem"
+    >
+      <div class="px-3">
+        <div style="height: 3rem;"></div>
+        <h3 class="text-center">Notes</h3>
+        <div class="row p-2">
+          <b-button
+            class="col-4 note-btn"
+            v-b-toggle.sidebar-backdrop
+            v-for="note in received_note_list"
+            :key="note.noteNo"
+            @click="getNoteHTML(note.noteNo)"
+          >
+            <h5>{{ note.title }}</h5>
+            <span>{{compute_date(note.createdDate)}}</span>
+          </b-button>
+        </div>
+      </div>
+    </b-sidebar>
   </b-col>
 </template>
 
 <script>
-import NoteSearchSidebar from './NoteSearchSidebar.vue';
-
 export default {
   name: 'NoteSearch',
-  components: {
-    NoteSearchSidebar,
-  },
+  components: {},
   props: {
     group_list: Array,
     received_note_list: Array,
@@ -30,12 +74,40 @@ export default {
     getNoteHTML(NoteId) {
       this.$emit('onGetNoteHTML', NoteId);
     },
+    compute_date(date) {
+      return date.slice(2,10);
+    }
   },
   data() {
     return {};
   },
 };
 </script>
-
-<style>
+<style >
+.nav {
+  padding: 0;
+}
+.v-navigation-drawer__content {
+  background-color: rgb(52, 63, 87);
+}
+#myProfile {
+  width: 100%;
+  text-align: center;
+}
+#myImage {
+  margin-top: 20px;
+  display: inline-block;
+  width: 100px;
+  height: 100px;
+  object-fit: cover;
+  border-radius: 50%;
+}
+.groupNameText {
+  color: white;
+}
+.note-btn:hover {
+  color: red !important;
+  border: 1px solid red;
+  background: red !important;
+}
 </style>
