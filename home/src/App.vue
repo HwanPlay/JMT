a42c21e648e51cd4bbe005fbf2503175752be89e
이태환 <lwh1992@naver.com>
2020-08-14 15:50:55 +0900

    init home

---
+++ home/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <JMTlogo />
+  </div>
+</template>
+
+<script>
+import JMTlogo from './components/JMTlogo.vue';
+
+export default {
+  name: 'App',
+  components: {
+    JMTlogo,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ home/src/components/JMTlogo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <img src="../assets/JMT_logo.jpg">
+    <span>Join & MeeTing</span>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
