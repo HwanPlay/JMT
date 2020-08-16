b53c73dfca2af49d24aa56ba2037de9b15dc3492
SangEun <eight34@naver.com>
2020-08-17 01:21:04 +0900

    Front | Group Members

--- front-end/src/components/Group/GroupMembers.vue
+++ front-end/src/components/Group/GroupMembers.vue
@@ -8,14 +8,15 @@
       <v-card>
         <v-card-title class="headline top d-flex justify-center align-center">그룹원</v-card-title>
           <div v-if="membersInfo && membersInfo.length===0">
-            그룹원들을 초대해주세요
+            <v-icon color="rgb(52, 63, 87);" class="d-flex justify-center align-center mt-8" size="100">far fa-dizzy</v-icon>
+            <h3 class="d-flex justify-center align-center mt-8">그룹원들이 없습니다</h3>
           </div>
           <v-card-text v-for="memberInfo in membersInfo" :key='memberInfo.id' style="padding: 5px;">
             <MemberCard :userInfo=memberInfo :groupNo=groupNo :hostId=hostId @refresh="refresh" />
           </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="green darken-1" text @click="dialog = false">
+          <v-btn color="rgb(255, 128, 74)" text @click="dialog = false">
             Close
           </v-btn>
         </v-card-actions>
