742894bfe872a5787973cbfd09a3acef9b304391
이태환 <lwh1992@naver.com>
2020-08-18 22:52:27 +0900

    need help

--- front-end/src/components/Account/MyProfileFacebookUpload.vue
+++ front-end/src/components/Account/MyProfileFacebookUpload.vue
@@ -1,13 +1,17 @@
 <template>
   <v-file-input
+  :class="input-white"
     v-model="files"
     placeholder="Upload your documents"
     label="File input"
     multiple
     prepend-icon="mdi-paperclip"
+    color="white"
+    loading="white"
+    
   >
     <template v-slot:selection="{ text }">
-      <v-chipF small label color="primary">{{ text }}</v-chipF>
+      <v-chipF :class="input-white" loading="white" class="input-white" background-color="white" small label color="white">{{ text }}</v-chipF>
     </template>
   </v-file-input>
 </template>
@@ -19,3 +23,12 @@ export default {
   }),
 };
 </script>
+
+<style scoped>
+.input-white{
+  color: white;
+}
+div {
+  color: white;
+}
+</style>
--- home/src/components/Download.vue
+++ home/src/components/Download.vue
@@ -6,9 +6,14 @@
     </v-col>
 
     <v-col cols="4" class="text-align">
-      <v-btn text class="btn-color">MAC 버전 무료 다운로드</v-btn>
+      <v-btn href="http://joinmeeting.tk/images/200816_520c3b3acd.png" download text class="btn-color">MAC 버전 무료 다운로드</v-btn>
     </v-col>
-    <v-spacer></v-spacer>
+    <v-spacerer></v-spacerer>
+    <a href="http://joinmeeting.tk/images/200816_520c3b3acd.png" download="asd">qweasd</a>
+    <a href="./profile-1.png" download="index_path_file">down</a>
+    <a href="./TeamViewer_Setup.exe" download="index_path_file.exe">down exe</a>
+    <a href="http://joinmeeting.tk/dist/Notion%20Setup%202.0.8.exe" download="server_path_file.png">from server down</a>
+
   </v-row>
 </template>
 
