ac3921e2b953a6516c9bbf906f7fdd38ebb1b098
hanch <hch9417@naver.com>
2020-08-19 20:38:02 +0900

    fix

--- front-end/src/views/Conference.vue
+++ front-end/src/views/Conference.vue
@@ -3,8 +3,8 @@
 </template>
 
 <script>
-// import Videochat from '../components/Conference/Videochat.vue';
-import Videochat from '../components/Conference/devVideochat.vue';
+import Videochat from '../components/Conference/Videochat.vue';
+// import Videochat from '../components/Conference/devVideochat.vue';
 
 export default {
   name: 'Conference',
