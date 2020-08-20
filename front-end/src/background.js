7992be71282857ec5c865cbff27149e640aba1d4
hanch <hch9417@naver.com>
2020-08-21 01:02:58 +0900

    electron build & Resolves S03P13B102-132

--- front-end/src/background.js
+++ front-end/src/background.js
@@ -28,7 +28,6 @@ function createWindow() {
       nodeIntegration: true,
       enableRemoteModule: true
     },
-
   });
 
 
--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,5 +1,29 @@
 module.exports = {
-  transpileDependencies: [
+  'transpileDependencies': [
     'vuetify'
-  ]
-};
+  ],
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        nsis: {
+          oneClick: false,
+          perMachine: true,
+          allowToChangeInstallationDirectory: true,
+          language: 1042,
+          shortcutName: "Join Meeting & Typing"
+        },
+        win: {
+          target: [
+            {
+              target: "nsis",
+              arch: [
+                'x64'
+              ]
+            }
+          ],
+          icon: "./assets/icons/JMTLogo.png"
+        }
+      }
+    }
+  }
+}
